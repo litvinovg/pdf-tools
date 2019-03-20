934c9cdf03202d8b88a683b36f15a0da4c458583
George Litvinov <git@litvinovg.pro>
2019-03-20 12:25:30 +0300

    Refactoring

--- src/main/java/ru/ras/iph/impose/ImposeonA3.java
+++ src/main/java/ru/ras/iph/impose/ImposeonA3.java
@@ -15,7 +15,11 @@ import com.itextpdf.text.pdf.PdfWriter;
 
 public class ImposeonA3 {
 	
-  
+	static int a5Height = Math.round(PageSize.A5.getHeight());
+	static int a5Width = Math.round(PageSize.A5.getWidth());
+	static int a4Height = Math.round(PageSize.A4.getHeight());
+	static int a4Width = Math.round(PageSize.A4.getWidth());
+	
     /**
      * Main method.
      * @param    args    no arguments needed
@@ -48,14 +52,9 @@ public class ImposeonA3 {
     	
         PdfReader reader = new PdfReader(is);
         Document document = new Document(PageSize.A3);
-        Rectangle pagesize = document.getPageSize();
         
-        float HeightA3 = pagesize.getHeight();
-        float WidthA3 = pagesize.getWidth();
-        PdfWriter writer
-            = PdfWriter.getInstance(document, os);
+        PdfWriter writer = PdfWriter.getInstance(document, os);
         document.open();
-        PdfContentByte canvas = writer.getDirectContent();
         PdfImportedPage page;
         int layout = 1;
         for (int i = 1; i <= reader.getNumberOfPages(); i = i + 8) {
@@ -63,245 +62,257 @@ public class ImposeonA3 {
         	
         	int curHeight = Math.round(page.getHeight());
         	int curWidth = Math.round(page.getWidth());
-        	int a5Height = Math.round(PageSize.A5.getHeight());
-        	int a5Width = Math.round(PageSize.A5.getWidth());
-        	System.out.println("Ширина страницы " + curWidth);
-  				System.out.println("Высота страницы " + curHeight);
-  				System.out.println("Ширина страницы А5 " + a5Width);
-  				System.out.println("Высота страницы A5 " + a5Height);
-  				
+
+        		
 			if ((curWidth <= a5Width && curHeight <= a5Height)||(curHeight <= a5Width && curWidth <= a5Height)) {
 			
-				float deltaWLayout = 1f;
-				float deltaHLayout = 1f;
-				float deltaW = 1f;
-				float deltaH = 1f;
-				if (page.getHeight() < page.getWidth()){
-					deltaW = (WidthA3 - 2 * page.getHeight()) / 2;
-					deltaH = (HeightA3 - 2 * page.getWidth()) / 2;
-					deltaWLayout = WidthA3 /2 - page.getHeight();
-					deltaHLayout = HeightA3/2 -  page.getWidth();
-				} else {
-					deltaW = (WidthA3 - 2 * page.getWidth()) / 2;
-					deltaH = (HeightA3 - 2 * page.getHeight()) / 2;
-					deltaWLayout = WidthA3 /2 - page.getWidth();
-					deltaHLayout = HeightA3/2 - page.getHeight();
-				}
-				drawFoldLines(canvas);
-				drawRectangle4(canvas);
-				drawSerifs4(canvas, layout);
-				// 1 Page
-				if (page.getWidth() > page.getHeight()){
-					canvas.addTemplate(page, 0, 1f, -1f, 0, WidthA3 - deltaWLayout,deltaHLayout );
-	        	} else {
-	        		canvas.addTemplate(page, 1f, 0, 0, 1, WidthA3 / 2, deltaH);
-				}
-				// 8 Page
-				if (i + 7 <= reader.getNumberOfPages()) {
-					page = writer.getImportedPage(reader, i + 7);
-					if (page.getWidth() > page.getHeight()){
-						canvas.addTemplate(page, 0, -1f, 1f, 0, deltaWLayout,HeightA3/2 );
-		        	} else {
-					canvas.addTemplate(page, 1f, 0, 0, 1f, deltaW, deltaH);
-		        	}
-				}
-				// 5 Page
-				if (i + 4 <= reader.getNumberOfPages()) {
-					page = writer.getImportedPage(reader, i + 4);
-					if (page.getWidth() > page.getHeight()){
-						canvas.addTemplate(page,  0, -1f, 1f, 0, deltaWLayout,HeightA3 - deltaHLayout );
-		        	} else {
-					canvas.addTemplate(page, -1f, 0, 0, -1f, WidthA3 / 2, HeightA3 - deltaH);
-		        	}
-				}
-				// 4 Page
-				if (i + 3 <= reader.getNumberOfPages()) {
-					page = writer.getImportedPage(reader, i + 3);
-					if (page.getWidth() > page.getHeight()){
-						canvas.addTemplate(page, 0, 1f, -1f, 0, WidthA3 - deltaWLayout,HeightA3/2 );
-		        	} else {
-					canvas.addTemplate(page, -1f, 0, 0, -1f, WidthA3 - deltaW, HeightA3 - deltaH);
-		        	}
-				}
+				layout = imposeA5onA3(reader, document, writer, page, layout, i);
 				
+			} else if ((curWidth<= a4Width && curHeight <= a4Height)||(curHeight <= a4Width && curWidth <= a4Height)) {
 				
-				//Second layout
-				//If there is at least one more page to place in new layout
-				if (i + 1 <= reader.getNumberOfPages()) {
-					document.newPage();
-					//next layout
-					layout++;
-					//Draw fold lines
-					drawFoldLines(canvas);
-					// 7 Page
-					if (i + 6 <= reader.getNumberOfPages()) {
-						page = writer.getImportedPage(reader, i + 6);
-						if (page.getWidth() > page.getHeight()){
-							canvas.addTemplate(page, 0, 1f, -1f, 0, WidthA3 - deltaWLayout,deltaHLayout );
-			        	} else {
-						canvas.addTemplate(page, 1f, 0, 0, 1, WidthA3 / 2, deltaH);
-			        	}
-					}
-					// Page 2
-					page = writer.getImportedPage(reader, i + 1);
-					if (page.getWidth() > page.getHeight()){
-						canvas.addTemplate(page, 0, -1f, 1f, 0, deltaWLayout,HeightA3/2 );
-		        	} else {
-					canvas.addTemplate(page, 1f, 0, 0, 1f, deltaW, deltaH);
-		        	}
-					// Page 3
-					if (i + 2 <= reader.getNumberOfPages()) {
-						page = writer.getImportedPage(reader, i + 2);
-						if (page.getWidth() > page.getHeight()){
-							canvas.addTemplate(page,  0, -1f, 1f, 0, deltaWLayout,HeightA3 - deltaHLayout );
-			        	} else {
-						canvas.addTemplate(page, -1f, 0, 0, -1f, WidthA3 / 2, HeightA3 - deltaH);
-			        	}
-					}
-					// Page 6
-					if (i + 5 <= reader.getNumberOfPages()) {
-						page = writer.getImportedPage(reader, i + 5);
-						if (page.getWidth() > page.getHeight()){
-							canvas.addTemplate(page, 0, 1f, -1f, 0, WidthA3 - deltaWLayout,HeightA3/2 );
-			        	} else {
-						canvas.addTemplate(page, -1f, 0, 0, -1f, WidthA3 - deltaW, HeightA3 - deltaH);
-			        	}
-					}
-				}
-				// New layout if any further pages exists
-				if (i + 8 <= reader.getNumberOfPages()) {
-					document.newPage();
-					//next layout
-					layout++;
-				}
+				layout = imposeA4onA3(reader, document, writer, page, layout, i);
 				
-				//If page is greater than A5 and less than A4
-			} else if ((page.getWidth() <= PageSize.A4.getWidth() && page.getHeight() <= PageSize.A4.getHeight())||(page.getHeight() <= PageSize.A4.getWidth() && page.getWidth() <= PageSize.A4.getHeight())) {
-				
-				float deltaWLayout = 1f;
-				float deltaHLayout = 1f;
-				float deltaW = 1f;
-				float deltaH = 1f;
-				if (page.getHeight() < page.getWidth()){
-					deltaW = (WidthA3 - page.getWidth()) / 2;
-					deltaH = HeightA3 / 2 - page.getHeight();
-					deltaWLayout = (WidthA3 - page.getWidth()) / 2;
-					deltaHLayout = HeightA3 / 2 - page.getHeight();
-					
-				} else {
-					deltaW = (WidthA3 - page.getHeight()) / 2;
-					deltaH = HeightA3 / 2 - page.getWidth();
-					deltaWLayout = (WidthA3 - page.getHeight()) / 2;
-					deltaHLayout = HeightA3 / 2 - page.getWidth();
-				}
-
-				drawRectangle2(canvas);
-				drawFoldLines(canvas);
-				drawSerifs2(canvas, layout);
-				// 1 Page
-				if (page.getWidth() > page.getHeight()){
-					canvas.addTemplate(page, 1f, 0, 0, 1f, deltaWLayout ,deltaHLayout );
-	        	} else {
-	        		canvas.addTemplate(page, 0, -1f, 1f, 0, deltaW, HeightA3 / 2);
-	        	}
-				// 4 Page
-				if (i + 3 <= reader.getNumberOfPages()) {
-					page = writer.getImportedPage(reader, i + 3);
-					if (page.getWidth() > page.getHeight()){
-						canvas.addTemplate(page, -1f, 0, 0, -1f, WidthA3 - deltaWLayout ,HeightA3 - deltaHLayout );
-		        	} else {
-					canvas.addTemplate(page, 0, -1f, 1f, 0, deltaW, HeightA3 - deltaH);
-		        	}
-				}
-				// Second layout
-				//If there is at least one more page to place in new layout
-				if (i + 1 <= reader.getNumberOfPages()) {
-					document.newPage();
-					//next layout
-					layout++;
-					drawFoldLines(canvas);
-					// 3 Page
-					if (i + 2 <= reader.getNumberOfPages()) {
-						page = writer.getImportedPage(reader, i + 2);
-						if (page.getWidth() > page.getHeight()){
-							canvas.addTemplate(page, 1f, 0, 0, 1f, deltaWLayout ,deltaHLayout );
-			        	} else {
-			        		canvas.addTemplate(page, 0, -1f, 1f, 0, deltaW, HeightA3 / 2);
-			        	}
-					}
-					// 2 Page
-					page = writer.getImportedPage(reader, i + 1);
-					if (page.getWidth() > page.getHeight()){
-						canvas.addTemplate(page, -1f, 0, 0, -1f, WidthA3 - deltaWLayout ,HeightA3 - deltaHLayout );
-		        	} else {
-					canvas.addTemplate(page, 0, -1f, 1f, 0, deltaW, HeightA3 - deltaH);
-		        	}
-				}
-				// Third layout
-				//If there is at least one more page to place in new layout
-				if (i + 4 <= reader.getNumberOfPages()) {
-					document.newPage();
-					//next layout
-					layout++;
-					drawRectangle2(canvas);
-					drawFoldLines(canvas);
-					drawSerifs2(canvas, layout);
-					// 5 Page
-					page = writer.getImportedPage(reader, i + 4);
-					if (page.getWidth() > page.getHeight()){
-						canvas.addTemplate(page, 1f, 0, 0, 1f, deltaWLayout ,deltaHLayout );
-		        	} else {
-		        		canvas.addTemplate(page, 0, -1f, 1f, 0, deltaW, HeightA3 / 2);
-		        	}
-					// 8 page
-					if (i + 7 <= reader.getNumberOfPages()) {
-						page = writer.getImportedPage(reader, i + 7);
-						if (page.getWidth() > page.getHeight()){
-							canvas.addTemplate(page, -1f, 0, 0, -1f, WidthA3 - deltaWLayout ,HeightA3 - deltaHLayout );
-			        	} else {
-						canvas.addTemplate(page, 0, -1f, 1f, 0, deltaW, HeightA3 - deltaH);
-			        	}
-					}
-				}
-				// Fourth layout
-				if (i + 5 <= reader.getNumberOfPages()) {
-					document.newPage();
-					//next layout
-					layout++;
-					drawFoldLines(canvas);
-					// Page 7
-					if (i + 6 <= reader.getNumberOfPages()) {
-						// 3
-						page = writer.getImportedPage(reader, i + 6);
-						if (page.getWidth() > page.getHeight()){
-							canvas.addTemplate(page, 1f, 0, 0, 1f, deltaWLayout ,deltaHLayout );
-			        	} else {
-			        		canvas.addTemplate(page, 0, -1f, 1f, 0, deltaW, HeightA3 / 2);
-			        	}
-					}
-					// Page 6
-					page = writer.getImportedPage(reader, i + 5);
-					if (page.getWidth() > page.getHeight()){
-						canvas.addTemplate(page, -1f, 0, 0, -1f, WidthA3 - deltaWLayout ,HeightA3 - deltaHLayout );
-		        	} else {
-					canvas.addTemplate(page, 0, -1f, 1f, 0, deltaW, HeightA3 - deltaH);
-		        	}
-
-				}
-				// New layout if any further pages exists
-				if (i + 8 <= reader.getNumberOfPages()) {
-					document.newPage();
-					//next layout
-					layout++;
-				}
 			}
 			
             
         }
-        // step 5
+
         document.close();
         reader.close();
     }
+	private static int imposeA4onA3(PdfReader reader, Document document, PdfWriter writer, PdfImportedPage page, int layout, int i) {
+		
+        PdfContentByte canvas = writer.getDirectContent();
+        float HeightA3 = document.getPageSize().getHeight();
+        float WidthA3 = document.getPageSize().getWidth();
+
+		float deltaWLayout = 1f;
+		float deltaHLayout = 1f;
+		float deltaW = 1f;
+		float deltaH = 1f;
+		if (page.getHeight() < page.getWidth()){
+			deltaW = (WidthA3 - page.getWidth()) / 2;
+			deltaH = HeightA3 / 2 - page.getHeight();
+			deltaWLayout = (WidthA3 - page.getWidth()) / 2;
+			deltaHLayout = HeightA3 / 2 - page.getHeight();
+			
+		} else {
+			deltaW = (WidthA3 - page.getHeight()) / 2;
+			deltaH = HeightA3 / 2 - page.getWidth();
+			deltaWLayout = (WidthA3 - page.getHeight()) / 2;
+			deltaHLayout = HeightA3 / 2 - page.getWidth();
+		}
+
+		drawRectangle2(canvas);
+		drawFoldLines(canvas);
+		drawSerifs2(canvas, layout);
+		// 1 Page
+		if (page.getWidth() > page.getHeight()){
+			canvas.addTemplate(page, 1f, 0, 0, 1f, deltaWLayout ,deltaHLayout );
+		} else {
+			canvas.addTemplate(page, 0, -1f, 1f, 0, deltaW, HeightA3 / 2);
+		}
+		// 4 Page
+		if (i + 3 <= reader.getNumberOfPages()) {
+			page = writer.getImportedPage(reader, i + 3);
+			if (page.getWidth() > page.getHeight()){
+				canvas.addTemplate(page, -1f, 0, 0, -1f, WidthA3 - deltaWLayout ,HeightA3 - deltaHLayout );
+			} else {
+			canvas.addTemplate(page, 0, -1f, 1f, 0, deltaW, HeightA3 - deltaH);
+			}
+		}
+		// Second layout
+		//If there is at least one more page to place in new layout
+		if (i + 1 <= reader.getNumberOfPages()) {
+			document.newPage();
+			//next layout
+			layout++;
+			drawFoldLines(canvas);
+			// 3 Page
+			if (i + 2 <= reader.getNumberOfPages()) {
+				page = writer.getImportedPage(reader, i + 2);
+				if (page.getWidth() > page.getHeight()){
+					canvas.addTemplate(page, 1f, 0, 0, 1f, deltaWLayout ,deltaHLayout );
+		    	} else {
+		    		canvas.addTemplate(page, 0, -1f, 1f, 0, deltaW, HeightA3 / 2);
+		    	}
+			}
+			// 2 Page
+			page = writer.getImportedPage(reader, i + 1);
+			if (page.getWidth() > page.getHeight()){
+				canvas.addTemplate(page, -1f, 0, 0, -1f, WidthA3 - deltaWLayout ,HeightA3 - deltaHLayout );
+			} else {
+			canvas.addTemplate(page, 0, -1f, 1f, 0, deltaW, HeightA3 - deltaH);
+			}
+		}
+		// Third layout
+		//If there is at least one more page to place in new layout
+		if (i + 4 <= reader.getNumberOfPages()) {
+			document.newPage();
+			//next layout
+			layout++;
+			drawRectangle2(canvas);
+			drawFoldLines(canvas);
+			drawSerifs2(canvas, layout);
+			// 5 Page
+			page = writer.getImportedPage(reader, i + 4);
+			if (page.getWidth() > page.getHeight()){
+				canvas.addTemplate(page, 1f, 0, 0, 1f, deltaWLayout ,deltaHLayout );
+			} else {
+				canvas.addTemplate(page, 0, -1f, 1f, 0, deltaW, HeightA3 / 2);
+			}
+			// 8 page
+			if (i + 7 <= reader.getNumberOfPages()) {
+				page = writer.getImportedPage(reader, i + 7);
+				if (page.getWidth() > page.getHeight()){
+					canvas.addTemplate(page, -1f, 0, 0, -1f, WidthA3 - deltaWLayout ,HeightA3 - deltaHLayout );
+		    	} else {
+				canvas.addTemplate(page, 0, -1f, 1f, 0, deltaW, HeightA3 - deltaH);
+		    	}
+			}
+		}
+		// Fourth layout
+		if (i + 5 <= reader.getNumberOfPages()) {
+			document.newPage();
+			//next layout
+			layout++;
+			drawFoldLines(canvas);
+			// Page 7
+			if (i + 6 <= reader.getNumberOfPages()) {
+				// 3
+				page = writer.getImportedPage(reader, i + 6);
+				if (page.getWidth() > page.getHeight()){
+					canvas.addTemplate(page, 1f, 0, 0, 1f, deltaWLayout ,deltaHLayout );
+		    	} else {
+		    		canvas.addTemplate(page, 0, -1f, 1f, 0, deltaW, HeightA3 / 2);
+		    	}
+			}
+			// Page 6
+			page = writer.getImportedPage(reader, i + 5);
+			if (page.getWidth() > page.getHeight()){
+				canvas.addTemplate(page, -1f, 0, 0, -1f, WidthA3 - deltaWLayout ,HeightA3 - deltaHLayout );
+			} else {
+			canvas.addTemplate(page, 0, -1f, 1f, 0, deltaW, HeightA3 - deltaH);
+			}
+
+		}
+		// New layout if any further pages exists
+		if (i + 8 <= reader.getNumberOfPages()) {
+			document.newPage();
+			//next layout
+			layout++;
+		}
+		return layout;
+	}
+	private static int imposeA5onA3(PdfReader reader, Document document, PdfWriter writer, PdfImportedPage page, int layout, int i) {
+		PdfContentByte canvas = writer.getDirectContent();
+        float HeightA3 = document.getPageSize().getHeight();
+        float WidthA3 = document.getPageSize().getWidth();
+
+		float deltaWLayout = 1f;
+		float deltaHLayout = 1f;
+		float deltaW = 1f;
+		float deltaH = 1f;
+		if (page.getHeight() < page.getWidth()){
+			deltaW = (WidthA3 - 2 * page.getHeight()) / 2;
+			deltaH = (HeightA3 - 2 * page.getWidth()) / 2;
+			deltaWLayout = WidthA3 /2 - page.getHeight();
+			deltaHLayout = HeightA3/2 -  page.getWidth();
+		} else {
+			deltaW = (WidthA3 - 2 * page.getWidth()) / 2;
+			deltaH = (HeightA3 - 2 * page.getHeight()) / 2;
+			deltaWLayout = WidthA3 /2 - page.getWidth();
+			deltaHLayout = HeightA3/2 - page.getHeight();
+		}
+		drawFoldLines(canvas);
+		drawRectangle4(canvas);
+		drawSerifs4(canvas, layout);
+		// 1 Page
+		if (page.getWidth() > page.getHeight()){
+			canvas.addTemplate(page, 0, 1f, -1f, 0, WidthA3 - deltaWLayout,deltaHLayout );
+		} else {
+			canvas.addTemplate(page, 1f, 0, 0, 1, WidthA3 / 2, deltaH);
+		}
+		// 8 Page
+		if (i + 7 <= reader.getNumberOfPages()) {
+			page = writer.getImportedPage(reader, i + 7);
+			if (page.getWidth() > page.getHeight()){
+				canvas.addTemplate(page, 0, -1f, 1f, 0, deltaWLayout,HeightA3/2 );
+			} else {
+			canvas.addTemplate(page, 1f, 0, 0, 1f, deltaW, deltaH);
+			}
+		}
+		// 5 Page
+		if (i + 4 <= reader.getNumberOfPages()) {
+			page = writer.getImportedPage(reader, i + 4);
+			if (page.getWidth() > page.getHeight()){
+				canvas.addTemplate(page,  0, -1f, 1f, 0, deltaWLayout,HeightA3 - deltaHLayout );
+			} else {
+			canvas.addTemplate(page, -1f, 0, 0, -1f, WidthA3 / 2, HeightA3 - deltaH);
+			}
+		}
+		// 4 Page
+		if (i + 3 <= reader.getNumberOfPages()) {
+			page = writer.getImportedPage(reader, i + 3);
+			if (page.getWidth() > page.getHeight()){
+				canvas.addTemplate(page, 0, 1f, -1f, 0, WidthA3 - deltaWLayout,HeightA3/2 );
+			} else {
+			canvas.addTemplate(page, -1f, 0, 0, -1f, WidthA3 - deltaW, HeightA3 - deltaH);
+			}
+		}
+		
+		
+		//Second layout
+		//If there is at least one more page to place in new layout
+		if (i + 1 <= reader.getNumberOfPages()) {
+			document.newPage();
+			//next layout
+			layout++;
+			//Draw fold lines
+			drawFoldLines(canvas);
+			// 7 Page
+			if (i + 6 <= reader.getNumberOfPages()) {
+				page = writer.getImportedPage(reader, i + 6);
+				if (page.getWidth() > page.getHeight()){
+					canvas.addTemplate(page, 0, 1f, -1f, 0, WidthA3 - deltaWLayout,deltaHLayout );
+		    	} else {
+				canvas.addTemplate(page, 1f, 0, 0, 1, WidthA3 / 2, deltaH);
+		    	}
+			}
+			// Page 2
+			page = writer.getImportedPage(reader, i + 1);
+			if (page.getWidth() > page.getHeight()){
+				canvas.addTemplate(page, 0, -1f, 1f, 0, deltaWLayout,HeightA3/2 );
+			} else {
+			canvas.addTemplate(page, 1f, 0, 0, 1f, deltaW, deltaH);
+			}
+			// Page 3
+			if (i + 2 <= reader.getNumberOfPages()) {
+				page = writer.getImportedPage(reader, i + 2);
+				if (page.getWidth() > page.getHeight()){
+					canvas.addTemplate(page,  0, -1f, 1f, 0, deltaWLayout,HeightA3 - deltaHLayout );
+		    	} else {
+				canvas.addTemplate(page, -1f, 0, 0, -1f, WidthA3 / 2, HeightA3 - deltaH);
+		    	}
+			}
+			// Page 6
+			if (i + 5 <= reader.getNumberOfPages()) {
+				page = writer.getImportedPage(reader, i + 5);
+				if (page.getWidth() > page.getHeight()){
+					canvas.addTemplate(page, 0, 1f, -1f, 0, WidthA3 - deltaWLayout,HeightA3/2 );
+		    	} else {
+				canvas.addTemplate(page, -1f, 0, 0, -1f, WidthA3 - deltaW, HeightA3 - deltaH);
+		    	}
+			}
+		}
+		// New layout if any further pages exists
+		if (i + 8 <= reader.getNumberOfPages()) {
+			document.newPage();
+			//next layout
+			layout++;
+		}
+		return layout;
+	}
  /**
  * @param canvas
  */
